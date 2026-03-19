d13853ff946b0e54c672f605803ad329f6f4949d
muradpm <abdulkadyr0v@yandex.ru>
2026-03-20 01:36:09 +0300

    feat: improve app theme bootstrapping

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import "./index.css"
-import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import "./index.css";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
-)
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+	throw new Error("Root element #root was not found");
+}
+
+createRoot(rootElement).render(
+	<StrictMode>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</StrictMode>,
+);
