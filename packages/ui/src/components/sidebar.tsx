c45b05821e2a24b5c464a15c791230f911dc6a33
muradpm <abdulkadyr0v@yandex.ru>
2026-04-21 23:59:28 +0300

    fix: improve tooltip hover timing

--- packages/ui/src/components/sidebar.tsx
+++ packages/ui/src/components/sidebar.tsx
@@ -661,7 +661,7 @@ function SidebarProvider({
 		<SidebarShellContext.Provider value={shellContextValue}>
 			<SidebarRightContext.Provider value={rightContextValue}>
 				<SidebarDockedPanelsContext.Provider value={dockedPanelsContextValue}>
-					<TooltipProvider delayDuration={0}>
+					<TooltipProvider>
 						<div
 							data-slot="sidebar-wrapper"
 							style={
